31967d1387dc9c67de6d942ca789554b9eea28f9
MateiEnache <enachem@wlhs.wlwv.k12.or.us>
2025-02-25 09:44:30 -0800

    changed and finished

--- tictactoe.py
+++ tictactoe.py
@@ -3,62 +3,86 @@ import random
 
 def board_create():
     chart = [
-    ["1 ","2 ","3 "],
-    ["4 ","5 ","6 "],
-    ["7 ","8 ","9 "]
-]
-    print(*chart[0])
-    print(*chart[1])
-    print(*chart[2])
+        ["1 ", "2 ", "3 "],
+        ["4 ", "5 ", "6 "],
+        ["7 ", "8 ", "9 "],
+    ]
+    print_board(chart)
     return chart
 
 def print_board(board):
     for row in board:
-        print(*row)
+        print(" ".join(row))
+    print()
 
 def start():
-    print("""
-          WELCOME TO TIC TAC TOE""")
+    print("WELCOME TO TIC-TAC-TOE")
     time.sleep(1)
 
 def game_loop(board):
     print("You are X and the enemy is O")
-
+    
     while True:
-        # Player move
         playdecision = input("Where would you like to place your piece? ") + " "
         
+        move_made = False
         for row in board:
             if playdecision in row:
                 row[row.index(playdecision)] = "X "
+                move_made = True
                 break
-
+        
+        if not move_made:
+            print("Invalid move, try again.")
+            continue
+        
         print_board(board)
+        
+        winner = check_winner(board)
+        if winner:
+            print(f"{winner} wins!")
+            return
+        
+        if check_draw(board):
+            print("It's a draw!")
+            return
 
-        # Computer move
         print("Computer is choosing...")
         time.sleep(1)
         comp_rand(board)
         print_board(board)
+        
+        winner = check_winner(board)
+        if winner:
+            print(f"{winner} wins!")
+            return
+        
+        if check_draw(board):
+            print("It's a draw!")
+            return
 
+def check_winner(board):
+    for row in board:
+        if row[0] == row[1] == row[2] and row[0] in ["X ", "O "]:
+            return row[0]
+    
+    for col in range(3):
+        if board[0][col] == board[1][col] == board[2][col] and board[0][col] in ["X ", "O "]:
+            return board[0][col]
+    
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] in ["X ", "O "]:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] in ["X ", "O "]:
+        return board[0][2]
+    
+    return None
 
-def game_main():
-    start()
-    time.sleep(1)
-    board = board_create()
-    game_loop(board)
-    print(*board[0])
-    print(*board[1])
-    print(*board[2])
-    comp_rand(board)
-    print("Computer is choosing")
-    time.sleep(1)
-    print("...")
-    time.sleep(1)
-    print(*board[0])
-    print(*board[1])
-    print(*board[2])
-    return board
+def check_draw(board):
+    for row in board:
+        for cell in row:
+            if cell not in ["X ", "O "]:
+                return False
+    return True
 
 def comp_rand(board):
     while True:
@@ -69,5 +93,16 @@ def comp_rand(board):
             board[row][col] = "O "
             break
 
-    
+def game_main():
+    while True:
+        start()
+        time.sleep(1)
+        board = board_create()
+        game_loop(board)
+        
+        play_again = input("Do you want to play again? (yes/no): ").strip().lower()
+        if play_again != "yes":
+            print("Thanks for playing!")
+            break
+
 game_main()
